afa3f7cbb8c37886b968ef8c7252152e959afab0
jovandamjanovic <jovandamjanovic@gmail.com>
2016-07-09 13:50:55 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Request-header-parser API mocroservice recovers the IP, language and operating system of the request sender.
+
+Project done for FreeCodeCamp basejump.
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+
+router.use(function(req, res, next) {
+    next();
+});
+
+router.route('/').get(function(req, res) {
+   var headerData = {};
+   headerData.ipaddress = req.headers['x-forwarded-for'];
+   headerData.language = req.headers['accept-language'].match(/([\W\w]+?),/)[1];
+   headerData.software = req.headers['user-agent'].match(/\((.+)\)/)[1];
+   res.json(headerData); 
+});
+
+app.use('/', router);
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Request header parser listening on port ' + (process.env.PORT || "8080"));
+});
